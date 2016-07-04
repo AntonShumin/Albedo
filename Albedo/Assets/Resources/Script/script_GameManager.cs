56b48e06c08410c6db1328d030216df142d3f3a9
AntonShumin <AntonShuminBe@gmail.com>
2016-07-04 17:53:22 +0200

    Rebuild scene, ambient settings camera settings

--- Albedo/Assets/Resources/Script/script_GameManager.cs
+++ Albedo/Assets/Resources/Script/script_GameManager.cs
@@ -4,5 +4,8 @@ using System.Collections;
 public class script_GameManager : script_Singleton<script_GameManager>
 {
 
-	
+    void Start()
+    {
+        Application.targetFrameRate = 90;
+    }
 }
