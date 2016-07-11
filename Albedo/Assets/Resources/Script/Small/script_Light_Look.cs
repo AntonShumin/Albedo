84322a61e97768592771405f99f19c63f3563446
AntonShumin <AntonShuminBe@gmail.com>
2016-07-11 22:19:45 +0200

    Material and double lighting setup

---
+++ Albedo/Assets/Resources/Script/Small/script_Light_Look.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class script_Light_Look : MonoBehaviour {
+
+    public GameObject Player;
+    private Transform player_transform;
+
+	// Use this for initialization
+	void Start ()
+    {
+        player_transform = Player.transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.LookAt(player_transform);
+	}
+}
