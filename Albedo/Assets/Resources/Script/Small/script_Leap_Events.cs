2285d6dd66d7dc98d495fbd6fadc503a859ac4cd
AntonShumin <AntonShuminBe@gmail.com>
2016-07-12 23:02:34 +0200

    Leap motion thumb extension test OK

---
+++ Albedo/Assets/Resources/Script/Small/script_Leap_Events.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class script_Leap_Events : MonoBehaviour {
+
+    public int testInt;
+
+    public void TestFinger()
+    {
+        Debug.Log("Thumb extension works");
+    }
+
+}
